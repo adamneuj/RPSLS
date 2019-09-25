777dba4b59f62b734469885c1baaa7421dd41fe3
Adam Neujahr <adamneujahr@gmail.com>
2019-09-25 13:33:28 -0500

    Added blank classes for Player, Human, and Computer

--- RPSLS/Game.cs
+++ RPSLS/Game.cs
@@ -8,5 +8,22 @@ namespace RPSLS
 {
     class Game
     {
+        //member variables (HAS A)
+        string rules;
+
+        //constructor
+        public Game()
+        {
+            //Display rules
+            //Player One selects either human or computer player
+            //Player Two selects either human or computer player
+            //Player One picks either Rock, Paper, Scissors, Lizard, or Spock
+            //Player Two picks either Rock, Paper, Scissors, Lizard, or Spock
+            //Determine winner of round
+            //Repeat until best two out of three
+            //Display winner
+
+        }
+        //member methods (CAN DO)
     }
 }
---
+++ RPSLS/Human.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPSLS
+{
+    class Human
+    {
+        // member variables
+
+        // constructor
+
+        // member methods
+    }
+}
